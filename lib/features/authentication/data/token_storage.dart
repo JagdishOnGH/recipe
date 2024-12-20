6eb1d15808e31a883921b5ead754a665c64d8e83
jagdish bhai <jagdissghimire@gmail.com>
2024-12-20 11:31:18 +0545

    abstraction of token storage

--- lib/features/authentication/data/token_storage.dart
+++ lib/features/authentication/data/token_storage.dart
@@ -1,23 +1,34 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class TokenStorage {
+abstract class TokenStorage {
+  Future<void> saveToken(String token);
+
+  Future<String?> getToken();
+
+  Future<void> clearToken();
+}
+
+class SharedPrefTokenStorage implements TokenStorage {
+  final SharedPreferences _prefs;
+
+  const SharedPrefTokenStorage(this._prefs);
+
   static const String _authTokenKey = 'user_auth_token';
+
+  get prefs => _prefs;
 
   // Save token
   Future<void> saveToken(String token) async {
-    final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_authTokenKey, token);
   }
 
   // Get token
   Future<String?> getToken() async {
-    final prefs = await SharedPreferences.getInstance();
     return prefs.getString(_authTokenKey);
   }
 
   // Remove token
   Future<void> clearToken() async {
-    final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_authTokenKey);
   }
 }
