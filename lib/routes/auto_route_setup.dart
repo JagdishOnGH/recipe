9703672254686082d05e7bc6ce90c1416120fe9b
jagdish bhai <jagdissghimire@gmail.com>
2024-12-21 21:45:22 +0545

    auto route is setup

---
+++ lib/routes/auto_route_setup.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:recipe_app/routes/auto_route_setup.gr.dart';
+
+@AutoRouterConfig()
+class AutoRouteSetup extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: LoginRoute.page, path: "/login"),
+        AutoRoute(
+            page: OnboardingRoute.page, path: "/onboarding", initial: true),
+      ];
+}
