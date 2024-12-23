b7bebf90a7d554001e29a7599f0c4efaff016f89
jagdish bhai <jagdissghimire@gmail.com>
2024-12-23 10:13:51 +0545

    made widget look more aesthetics

--- lib/features/present_recipe/presentation/pages/home_page.dart
+++ lib/features/present_recipe/presentation/pages/home_page.dart
@@ -132,25 +132,23 @@ class HomePage extends ConsumerWidget {
         ]));
   }
 
-  Widget IconAndInfo(IconData icon, String text) {
-    return Builder(builder: (context) {
-      return Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-        decoration: BoxDecoration(
-          color: Colors.yellow.shade50,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          children: [
-            Icon(
-              icon,
-              size: 20,
-              color: Colors.orange,
-            ),
-            Text(text)
-          ],
-        ),
-      );
-    });
-  }
+Widget IconAndInfo(IconData icon, String text) {
+  return Builder(builder: (context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      decoration: BoxDecoration(
+        //   color: Colors.yellow.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            size: 20,
+          ),
+          Text(text)
+        ],
+      ),
+    );
+  });
 }
