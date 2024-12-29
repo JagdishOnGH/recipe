82e0202d5ff463c45e3fe5d694e8108311380ea7
jagdish bhai <jagdissghimire@gmail.com>
2024-12-29 11:20:30 +0545

    update: mocked sharedpref and passed test

--- test/unit/auth_unit_test.dart
+++ test/unit/auth_unit_test.dart
@@ -1,17 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:recipe_app/di/getit_setup.dart';
+import 'package:recipe_app/exceptions/AppGlobalException.dart';
 import 'package:recipe_app/features/authentication/data/auth_datasource.dart';
+import 'package:recipe_app/features/authentication/data/token_storage.dart';
 import 'package:recipe_app/features/authentication/repository/auth_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   // TestWidgetsFlutterBinding.ensureInitialized();
 
+  SharedPreferences.setMockInitialValues({});
+  await setupGetIt();
+  final pref = await SharedPreferences.getInstance();
   group("Auth-Module", () {
-    setupGetIt();
     final dio = sl<Dio>();
     late AuthRepository authRepository;
     late AuthDatasource authDatasource;
+    late TokenStorage tokenStorage;
     setUp(() {
       authDatasource = RestDummyAuthDatasource(dio, sl());
       authRepository = AuthRepository(authDatasource);
