7ac4c2147fd7c4b84968ea4e9a981b815cf21fb7
jagdish bhai <jagdissghimire@gmail.com>
2024-12-29 11:21:06 +0545

    update: removed redundant classes

--- lib/enums/authentication_exceptions_enums.dart
+++
@@ -1,6 +0,0 @@
-enum AuthenticationExceptionsEnums {
-  EmailError,
-  PasswordError,
-  OtherError,
-  ServerError,
-}
--- lib/exceptions/authentication_exception.dart
+++
@@ -1,8 +0,0 @@
-import 'package:recipe_app/enums/authentication_exceptions_enums.dart';
-
-class AuthenticationException implements Exception {
-  final AuthenticationExceptionsEnums error;
-  final String? message;
-
-  AuthenticationException(this.message, {required this.error});
-}
