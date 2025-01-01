a938ce806be6589010975bfe0c6ed20130fc4af6
jagdish bhai <jagdissghimire@gmail.com>
2025-01-01 17:56:14 +0545

    feat: bottom navigation bar added.

---
+++ lib/features/present_recipe/presentation/pages/entry_point_page.dart
@@ -0,0 +1,33 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_app/routes/auto_route_setup.gr.dart';
+
+@RoutePage()
+class EntryPointPage extends StatelessWidget {
+  const EntryPointPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoTabsScaffold(
+      //  homeIndex: 0,
+      routes: const [HomeRoute(), LoginReminderRoute()],
+      bottomNavigationBuilder: (context, _) {
+        final tabsRouter = context.tabsRouter;
+        return BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: tabsRouter.activeIndex,
+          onTap: tabsRouter.setActiveIndex,
+        );
+      },
+    );
+  }
+}
