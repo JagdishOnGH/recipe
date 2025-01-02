0e591c44c18e927dc395e35e799a943e54c6cb67
jagdish bhai <jagdissghimire@gmail.com>
2025-01-02 20:29:36 +0545

    feat:user profile data working as expected.

---
+++ lib/features/authentication/presentation/riverpod/profile_displayer_rp.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipe_app/exceptions/app_global_exception.dart';
+import 'package:recipe_app/features/authentication/models/user_model.dart';
+import 'package:recipe_app/features/authentication/presentation/riverpod/authentication_rp.dart';
+import 'package:recipe_app/features/authentication/repository/auth_repository.dart';
+import 'package:recipe_app/helper/placeholder_class.dart';
+
+import '../../../../di/getit_setup.dart';
+
+AutoDisposeFutureProvider<UserModel> profileInformationProvider =
+    FutureProvider.autoDispose((ref) async {
+  final AsyncValue<DataPlaceHolder<String>> authState =
+      ref.watch(authenticationRpProvider);
+  if (authState.hasValue && authState.asData != null) {
+    final AuthRepository authRepo = sl();
+    AsyncLoading();
+    final data = authRepo.userProfile();
+    return data;
+  }
+  throw AppGlobalException("You are not logged in");
+});
