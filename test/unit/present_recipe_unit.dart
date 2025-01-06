a0b3938ae1f0f88d6ea142f307ccceb9d2667200
jagdish bhai <jagdissghimire@gmail.com>
2025-01-06 12:38:07 +0545

    chore: unit test for some functions under present recipe

--- test/unit/present_recipe_unit.dart
+++ test/unit/present_recipe_unit.dart
@@ -1,15 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:recipe_app/di/getit_setup.dart';
+import 'package:recipe_app/features/present_recipe/data/offline_datasource.dart';
 import 'package:recipe_app/features/present_recipe/data/recipe_datasource.dart';
 import 'package:recipe_app/features/present_recipe/models/recipe_model.dart';
+import 'package:recipe_app/features/present_recipe/repository/offline_recipe_repository.dart';
 import 'package:recipe_app/features/present_recipe/repository/recipe_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   // TestWidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences.setMockInitialValues({});
+  await setupGetIt();
 
   group("Present Recipi", () {
-    setupGetIt();
     final dio = sl<Dio>();
     late RecipeRepository recipeRepository;
     late RecipeDatasource recipeDatasource;
@@ -30,6 +34,23 @@ void main() {
       expect(
         count,
         equals(20),
+      );
+    });
+  });
+  group("offline-recipe", () {
+    late OfflineRecipeRepository recipeRepository;
+    late OfflineDatasource offlineDatasource;
+
+    setUp(() {
+      offlineDatasource = SharedPrefOfflineDatasource(sl<SharedPreferences>());
+      recipeRepository = OfflineRecipeRepository(offlineDatasource);
+    });
+    //test has recipe
+    test("Has Recipe", () async {
+      final res = await recipeRepository.hasRecipe(1);
+      expect(
+        res.runtimeType,
+        equals(bool),
       );
     });
   });
