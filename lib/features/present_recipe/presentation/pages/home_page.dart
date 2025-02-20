0ed54176d3288f41e3b33224ca0fccdd191f9f5c
jagdish bhai <jagdissghimire@gmail.com>
2025-02-20 18:32:23 +0545

    chore: shimmer effect added to app

--- lib/features/present_recipe/presentation/pages/home_page.dart
+++ lib/features/present_recipe/presentation/pages/home_page.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipe_app/exceptions/app_global_exception.dart';
 import 'package:recipe_app/extensions/on_num.dart';
-import 'package:shimmer/shimmer.dart';
 
 import '../../../../extensions/error_widget.dart';
 import '../../../../routes/auto_route_setup.gr.dart';
 import '../../../authentication/presentation/riverpod/authentication_rp.dart';
-import '../components/display_recipe_comp.dart';
+import '../components/items_list_comp.dart';
+import '../components/suggested_cuisine_comp.dart';
 import '../riverpod/present_recipe_rp.dart';
+import 'home_page_shimmer.dart';
 
 part '../components/icon_info_comp.dart';
 
@@ -17,6 +18,7 @@ part '../components/icon_info_comp.dart';
 class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
+    final ts = Theme.of(context).textTheme;
     final recipeList = ref.watch(presentRecipeRpProvider);
     final watchAuthentication = ref.watch(authenticationRpProvider);
 
@@ -36,13 +38,98 @@ class HomePage extends ConsumerWidget {
                 skipLoadingOnReload: false,
                 data: (data) {
                   if (!data.hasData) return Text("No Data Found");
-                  final actualData = data.data!;
-                  return ListView.builder(
-                      itemCount: actualData.recipes.length,
-                      itemBuilder: (context, index) {
-                        return DisplayRecipeComp(
-                            context, actualData.recipes[index]);
-                      });
+                  return ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    children: [
+                      Row(),
+                      10.ht,
+                      Center(
+                        child: Text(
+                          "Delicious recipes at your fingertips, anytime, anywhere!",
+                          style: ts.titleMedium?.copyWith(
+                              color: Colors.amber.shade900, fontSize: 13),
+                        ),
+                      ),
+                      20.ht,
+                      Divider(),
+                      10.ht,
+                      SuggestedCuisineComp(
+                        suggestedCuisine: [
+                          "Spanish",
+                          "Indian",
+                          "Nepali",
+                          "Italian",
+                        ],
+                      ),
+                      20.ht,
+                      Center(
+                        child: Text(
+                            "Do You Know? You can save recipe to view offline"),
+                      ),
+                      30.ht,
+                      Row(
+                        children: [
+                          Text("For You",
+                              style: ts.titleMedium?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54,
+                                  fontSize: 18)),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 1.0),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              size: 25,
+                            ),
+                          )
+                        ],
+                      ),
+                      10.ht,
+                      SizedBox(
+                        height: 195,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            ItemsComp(),
+                            ItemsComp(),
+                            ItemsComp(),
+                            ItemsComp(),
+                          ],
+                        ),
+                      ),
+                      20.ht,
+                      Row(
+                        children: [
+                          Text("Popular Recipes",
+                              style: ts.titleMedium?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54,
+                                  fontSize: 18)),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 1.0),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              size: 25,
+                            ),
+                          )
+                        ],
+                      ),
+                      10.ht,
+                      SizedBox(
+                        height: 210,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            ItemsComp(),
+                            ItemsComp(),
+                            ItemsComp(),
+                            ItemsComp(),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
                 },
                 error: (e, s) => CustomErrorWidget(
                       message: (e as AppGlobalException).message,
@@ -51,56 +138,7 @@ class HomePage extends ConsumerWidget {
                       },
                     ),
                 loading: () {
-                  return ListView.builder(
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          padding: const EdgeInsets.all(8),
-                          margin: const EdgeInsets.all(10),
-                          height: 300,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade200,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Center(
-                                child: Image.asset(
-                                  "assets/food_placeholder.png",
-                                  height: 120,
-                                  width: 120,
-                                ),
-                              ),
-                              30.ht,
-                              SizedBox(
-                                height: 20,
-                                width: 300,
-                                child: Shimmer.fromColors(
-                                  baseColor: Colors.grey.shade400,
-                                  highlightColor: Colors.grey.shade200,
-                                  child: Container(
-                                    color: Colors.grey.shade300,
-                                  ),
-                                ),
-                              ),
-                              10.ht,
-                              SizedBox(
-                                height: 20,
-                                child: Shimmer.fromColors(
-                                  baseColor: Colors.grey.shade400,
-                                  highlightColor: Colors.grey.shade200,
-                                  child: Container(
-                                    color: Colors.grey.shade300,
-                                  ),
-                                ),
-                              ),
-                              20.ht,
-                            ],
-                          ),
-                        );
-                      });
+                  return RecipeShimmerPage();
                 })));
   }
 }
